523addc0ef6246e71e59987103e253b2ea4dbd80
梁亚辉 <liangyahui@didiglobal.com>
2020-11-17 16:58:25 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <button @click="clickHandle1">点我1</button>
     <button @click="clickHandle2">点我2</button>
     <button @click="clickHandle3">点我3</button>
+    <button @click="clickHandle4">点我4</button>
     <Test-Comp />
     <Test-Comp v-if="flg === 1" />
   </div>
@@ -36,6 +37,9 @@ export default {
     clickHandle3() {
       this.dispatch({ type: 2, action: 2 });
     },
+    clickHandle4() {
+      this.dispatch({ type: 2, action: 1 });
+    }
   },
 };
 </script>
